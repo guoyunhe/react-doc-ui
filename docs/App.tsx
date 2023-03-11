0ff0f21a7c16cfcb7495d5c0552117e7a19afbf3
Guo Yunhe <i@guoyunhe.me>
2023-03-11 21:10:01 +0800

    doc

--- docs/App.tsx
+++ docs/App.tsx
@@ -7,8 +7,41 @@ import {
   DocProvider,
   DocTabCodeBlock,
 } from 'react-doc-ui';
-// eslint-disable-next-line import/default
-import exampleSource from './example.jsx?raw';
+
+const example = `
+import {
+  DocCodeBlock,
+  DocContainer,
+  DocDemoBlock,
+  DocHeader,
+  DocHeading,
+  DocProvider,
+} from 'react-doc-ui';
+
+function MyComponent({ children }) {
+  return <div>{children}</div>;
+}
+
+function App() {
+  return (
+    <DocProvider>
+      <DocContainer>
+        <DocHeader title="My Package" />
+        <p>My package description...</p>
+
+        <DocHeading>Install</DocHeading>
+        <DocCodeBlock language="bash" code="npm i my-package" />
+
+        <DocHeading>Usage</DocHeading>
+        <DocDemoBlock>
+          <MyComponent />
+        </DocDemoBlock>
+        <DocCodeBlock language="jsx" code={source} />
+      </DocContainer>
+    </DocProvider>
+  );
+}
+`;
 
 const tabExample = `
 <DocTabCodeBlock
@@ -53,7 +86,7 @@ export default function App() {
 
         <DocHeading>Usage</DocHeading>
 
-        <DocCodeBlock language="jsx" code={exampleSource} />
+        <DocCodeBlock language="jsx" code={example} />
 
         <DocHeading>DocProvider</DocHeading>
         <p>Wrap the whole React app</p>
