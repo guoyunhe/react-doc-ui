06ca5c06bdae827ca537839c5bd5b17e6b1f0ad2
Guo Yunhe <i@guoyunhe.me>
2023-03-12 14:15:00 +0800

    tests

---
+++ src/DocContainer.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import { DocContainer } from './DocContainer';
+
+describe('<DocContainer/>', () => {
+  it('render', async () => {
+    render(<DocContainer>Hello</DocContainer>);
+
+    const elem = await screen.findByText('Hello');
+
+    expect(elem.className).toBe('DocContainer');
+  });
+});
